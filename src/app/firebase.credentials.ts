f064d9bac56cb4654d3515acfd3e71fc31d662d9
Sharon Clark <clarksl@gmail.com>
2018-07-17 11:13:19 -0600

    firebase credentials and connection added

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBwTHWKev4sxTTSJeO0Zlc6BYwWW8F5DUI",
+    authDomain: "fbtest-7ae45.firebaseapp.com",
+    databaseURL: "https://fbtest-7ae45.firebaseio.com",
+    projectId: "fbtest-7ae45",
+    storageBucket: "fbtest-7ae45.appspot.com",
+    messagingSenderId: "1021977037435"
+  };
