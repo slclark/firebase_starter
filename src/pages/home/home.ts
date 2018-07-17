9c6f01ee5c91c92a3aa04818458388f6c0c0eda9
Sharon Clark <clarksl@gmail.com>
2018-07-17 14:03:52 -0600

    working read from firebaes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from './../../services/shopping-list/shopping-list.service';
+import { Item } from './../../models/item/item.model';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
 
 @IonicPage()
 @Component({
@@ -7,9 +11,39 @@ import { NavController, IonicPage } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  shoppingList$: Observable<Item[]>;
+  //shoppingList: Array<Item>= [];
+  constructor(public navCtrl: NavController, private shopping: ShoppingListService) {
+    this.shoppingList$ = this.shopping
+      .getShoppingList()
+      .snapshotChanges()
+      .map(
+        changes => {
+          return changes.map(c => ({
+            key: c.payload.key, ...c.payload.val()
+          }))
+        }
+      );
+/*
+      this.shoppingList$ = this.shopping
+        .getShoppingList()
+        .valueChanges()
+        .subscribe((datas) => {
+          console.log('sadf');
+            console.log("datas",datas);
+        },(err)=>{
+          console.log('asdf');
+          console.log("problems: ",err);
+        }
+      );
+*/
+      console.log(this.shoppingList$);
+      /*let item = {name:"test"};
+      this.shoppingList.push(item);
+      */
   }
 
+  ionViewDidLoad() {
+    console.log(this.shoppingList$);
+  }
 }
