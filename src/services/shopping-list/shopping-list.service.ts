a3792c33b790054b51680e8e7642a0ecfaf3a92f
Sharon Clark <clarksl@gmail.com>
2018-07-17 12:30:40 -0600

    include edit and delete functions

--- src/services/shopping-list/shopping-list.service.ts
+++ src/services/shopping-list/shopping-list.service.ts
@@ -14,4 +14,12 @@ export class ShoppingListService {
   addItem(item: Item) {
     return this.shoppingListRef.push(item);
   }
+
+  editItem(item: Item){
+    return this.shoppingListRef.update(item.key, item);
+  }
+
+  removeItem(item: Item){
+    return this.shoppingListRef.remove(item.key);
+  }
 }
